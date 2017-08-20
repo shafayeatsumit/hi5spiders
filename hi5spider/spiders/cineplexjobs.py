e0917dbe09fa617bf75b5984b312f97052455342
shafayeatsumit <shafayeatkabir@gmail.com>
2017-08-21 02:43:20 +0600

    db insert script added for cineplex

--- hi5spider/spiders/cineplexjobs.py
+++ hi5spider/spiders/cineplexjobs.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from geopy import geocoders
 import time
 import re
+from hi5spider.insert_cineplex import insert_jobs
+import psycopg2
 
 class CineplexJobSpider (CrawlSpider):
     name = "cineplexcrawler"
@@ -25,6 +27,44 @@ class CineplexJobSpider (CrawlSpider):
             yield response.follow(job, self.parse_job)
 
     def parse_job(self, response):
+        conn = psycopg2.connect("")
+        def province_field_number(province_name):
+            provinces = {
+                "Alberta" : "1",
+                "Manitoba" : "3",
+                "British Columbia" : "2",
+                "New Brunswick" : "4",
+                "Newfoundland" : "5",
+                "Nova Scotia" : "7",
+                "Northwest Territories" : "6",
+                "Nunavut" : "8",
+                "Ontario" : "9",
+                "Prince Edward Island" : "13",
+                "Quebec" : "10",
+                "Québec" : "10",
+                "Saskatchewan" : "11",
+                "Yukon"  : "12"
+            }
+            if province_name in provinces:
+                return provinces[province_name]
+            else:
+                return None
+
+        def get_lat_lon(address,city,province):
+            g = geocoders.GoogleV3(api_key='AIzaSyCZsl42Ue3KaTlNjrhk3WJG1475pugV42g')
+            location = None
+            location = g.geocode("{0} {1} {2} Canada".format(address,city,province), timeout=20)
+            if location:
+                lat, lon = location.latitude, location.longitude
+                postal_code_regex = re.search(r'[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d',location.address)
+                if postal_code_regex:
+                    postal_code = postal_code_regex.group(0)
+                else:
+                    postal_code = None    
+                return lat,lon,postal_code
+            else:
+                return None, None, None  
+
         def clean_html(soup):
             for i in soup.find_all(['script','span']):
                 i.extract()
@@ -48,12 +88,17 @@ class CineplexJobSpider (CrawlSpider):
         clean_html(soup)
         soup.find('h1').extract()
         job_description = str(soup).replace("\n","")
-        yield {
+        lat , lon, postal_code = get_lat_lon(address1,city,province)
+        data= {
             'job_title': title_text,
-            'province' : province,
+            'province' : province_field_number(province.strip()),
             'job_description' : job_description,
             'web_link' : response.url,
             'city' : city,
             'postal_code' : None,
-            'address1' : address1
-        }        
+            'address1' : address1,
+            'lat' : lat,
+            'lon' : lon,
+            'postal_code' : postal_code
+        }
+        insert_jobs(data, conn)        
